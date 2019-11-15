963b4e1b37e847c9184daa1c408d5754c0e9fba3
unknown <rifercor90@gmail.com>
2019-11-15 08:25:37 +0100

    ipvalidationkata

---
+++ IpValidationKata/IpAddressValidator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace IpValidationKata
+{
+    public class IpAddressValidator
+    {
+        public bool Validate(string ipAddress)
+        {
+            var segments = ipAddress.Split('.');
+            var isValidIp = true;
+            if (IsIpInvalid(segments))
+            {
+                isValidIp= false;
+            }
+            return isValidIp;
+        }
+
+        private bool IsIpInvalid(string[] segments)
+        {
+            var validNumberOfSegments = segments.Length == 4;
+            var lastByte = segments[segments.Length - 1];
+            return !validNumberOfSegments || IsInvalidLastByte(lastByte) || ContainsInvalidSegment(segments);
+        }
+
+        private bool ContainsInvalidSegment(string[] segments)
+        {
+            foreach (var segment in segments)
+            {
+                if (SegmentIsNotNumeric(segment) || NumberInSegmentNotAllowed(segment))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool NumberInSegmentNotAllowed(string segment)
+        {
+            var isNumberOutOfRange = 0 < Int32.Parse(segment) || Int32.Parse(segment) > 255;
+            return isNumberOutOfRange;
+        }
+
+        private bool SegmentIsNotNumeric(string segment)
+        {
+            var isNotNumeric = !Int32.TryParse(segment, out var segmentNumeric);
+            return isNotNumeric;
+        }
+
+        private bool IsInvalidLastByte(string lastByte)
+        {
+            return lastByte == "0" || lastByte == "255";
+        }
+    }
+}
---
+++ UnitTestProject1/IpAddressValidatorShouldDo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using IpValidationKata;
+using NUnit.Framework;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class IpAddressValidatorShouldDo
+    {   
+        [TestCase("1.1.1.0")]
+        [TestCase("1.1.1.255")]
+        [TestCase("a.a.a.a")]
+        [TestCase("1.1.1.1.1")]
+        [TestCase("256.0.0.1")]
+        [TestCase("1,1.1.1.1")]
+        [TestCase("....")]
+        [TestCase("1. 1. 1. 1 ")]
+        [TestCase("ø.1.1.2")]
+        [Test]
+        public void Validate_InvalidIp_ReturnFalse(string ipAddress)
+        {
+            IpAddressValidator validator = new IpAddressValidator();
+            var isValidIp = validator.Validate(ipAddress);
+            Assert.IsFalse(isValidIp);
+        }
+
+    }
+}
